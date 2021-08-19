e9d74b8ac54bb9f441b122c40a25b5861df29003
ceilingrat <67766223+ceilingrat@users.noreply.github.com>
2021-08-19 03:42:49 +0000

    install "github-pages" ruby package create "docs" directory create "index.md" and "_config.yml" in docs directory create "run.sh" to run jekyll from docs directory create ".replit" to set repl to "bash" type and run "run.sh"

---
+++ docs/index.md
@@ -0,0 +1,8 @@
+# test
+hello? Is this thing on?
+
+## maybe not?
+or
+
+## maybe??
+I don't know
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# insert command to run jekyll
+
+bundler exec jekyll s -s "docs" -H 0.0.0.0
