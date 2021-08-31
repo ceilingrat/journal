45ad8a8b0a8fb81c591629b0f2474d4e260e9a04
ceilingrat <67766223+ceilingrat@users.noreply.github.com>
2021-08-31 05:10:36 +0000

    modify "pub-post.sh" script to catch input errors

--- pub-post.sh
+++ pub-post.sh
@@ -5,19 +5,21 @@ INFILE=$(find $INPATH -printf "%f")
 if [ ! $INFILE ]
 then
   echo "error with input file"
+else
+
+  TODAY=$(TZ="/usr/share/zoneinfo/Japan" date "+%Y-%m-%d")
+
+  BASEDIR=$HOME/journal/docs
+  DRAFTSDIR=$BASEDIR/_drafts
+  POSTSDIR=$BASEDIR/_posts
+
+  COUNT=$(find $POSTSDIR -iname "*$TODAY*" | wc -l)
+
+  NAMEPRE=$(printf "$TODAY-%02d-" $COUNT)
+
+  NAME=${NAMEPRE}$INFILE
+
+  git mv $INPATH $POSTSDIR/$NAME
+  git add $POSTSDIR/$NAME
+
 fi
-
-TODAY=$(TZ="/usr/share/zoneinfo/Japan" date "+%Y-%m-%d")
-
-BASEDIR=$HOME/journal/docs
-DRAFTSDIR=$BASEDIR/_drafts
-POSTSDIR=$BASEDIR/_posts
-
-COUNT=$(find $POSTSDIR -iname "*$TODAY*" | wc -l)
-
-NAMEPRE=$(printf "$TODAY-%02d-" $COUNT)
-
-NAME=${NAMEPRE}$INFILE
-
-git mv $INPATH $POSTSDIR/$NAME
-git add $POSTSDIR/$NAME
